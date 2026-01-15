63905e3cff2452b2f1177263808359bd68f559e4
Adshyan Matheetharan <adshyan.20220528@iit.ac.lk>
2026-01-16 03:40:34 +0530

    feat(rooms): initialized Elysia and created API routes for room generations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { client } from "@/lib/client"
+import { useMutation } from "@tanstack/react-query"
 import { nanoid } from "nanoid"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 const ANIMALS = ["Lion", "Tiger", "Dog", "Cat"]
@@ -14,6 +17,7 @@ const generateUsername = () => {
 export default function Home() {
 
   const [username, setUsername] = useState("");
+  const router = useRouter()
 
   useEffect(() => {
     const main = () => {
@@ -31,6 +35,16 @@ export default function Home() {
 
     main()
   }, [])
+
+  const {mutate: createRoom} = useMutation({
+    mutationFn: async () => {
+      const res = await client.room.create.post()
+
+      if (res.status === 200) {
+        router.push(`/room/${res.data?.roomId}`)
+      }
+    }
+  })
 
   return <main className = "flex min-h-screen flex-col items-center justify-center p-4">
     <div className="w-full max-w-md space-y-8">
@@ -50,7 +64,7 @@ export default function Home() {
           </div>
         </div>
 
-        <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">CREATE MATCHSTICK ROOM</button>
+        <button onClick= {() => createRoom()} className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">CREATE MATCHSTICK ROOM</button>
       </div>
       </div>
     </div>
---
+++ src/app/room/[roomId]/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+    return <p>Hello Room</p>
+}
+
+export default Page
