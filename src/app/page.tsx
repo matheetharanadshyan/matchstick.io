1dbb4e2a1b11d755388d55782ec2781bb68e6107
Adshyan Matheetharan <adshyan.20220528@iit.ac.lk>
2026-01-16 02:26:52 +0530

    feat(home): created the user interface for the home layout of the application with mock username data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,58 @@
+"use client"
+
+import { nanoid } from "nanoid"
+import { useEffect, useState } from "react"
+
+const ANIMALS = ["Lion", "Tiger", "Dog", "Cat"]
+const STORAGE_KEY = "chat_username"
+
+const generateUsername = () => {
+  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)]
+  return `Anonymous-${word}-${nanoid(5)}`
+}
+
 export default function Home() {
-  return null;
+
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const main = () => {
+      const stored = localStorage.getItem(STORAGE_KEY)
+
+      if(stored) {
+        setUsername(stored)
+        return
+      }
+
+      const generated = generateUsername()
+      localStorage.setItem(STORAGE_KEY, generated)
+      setUsername(generated)
+    }
+
+    main()
+  }, [])
+
+  return <main className = "flex min-h-screen flex-col items-center justify-center p-4">
+    <div className="w-full max-w-md space-y-8">
+
+      <div className="text-center space-y-5 pb-5">
+        <h1 className="text-3xl font-bold text-orange-400">Matchstick.io</h1>
+        <p> A Private Encrypted Self-Destructing Chat Room</p>
+      </div>
+      <div className="border border-zinc-700 background-zinc-900/50 p-6 backdrop-blur-md">
+      <div className="space-y-5">
+        <div className="space-y-2">
+          <label className="flex items-center text-zinc-400 pb-4">Your Identity</label>
+          <div className="flex items-center gap-5">
+            <div className="flex-1 bg-zinc-950 border border-zinc-800 p-4 text-sm text-zinc-300 font-mono mb-2">
+              {username}
+            </div>
+          </div>
+        </div>
+
+        <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">CREATE MATCHSTICK ROOM</button>
+      </div>
+      </div>
+    </div>
+  </main>
 }
