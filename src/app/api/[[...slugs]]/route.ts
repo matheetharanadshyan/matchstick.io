31bf314fec4e9425a3a791b2cc8bb856892b27f4
Adshyan Matheetharan <adshyan.20220528@iit.ac.lk>
2026-01-17 01:32:58 +0530

    feat(rooms): created a middleware and authentication system for the API endpoints

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from '@/lib/redis'
 import { Elysia, t } from 'elysia'
 import { nanoid } from 'nanoid'
+import { authMiddleware } from './auth'
+import { z }  from 'zod'
 
 const ROOM_TTL_SECONDS = 60 * 10
 
@@ -17,7 +19,24 @@ const rooms = new Elysia({prefix: '/room'} ).post('/create', async () => {
     return { roomId }
 })
 
-const app = new Elysia({ prefix: '/api' }).use(rooms)
+const messages = new Elysia({ prefix: '/messages'}).use(authMiddleware).post("/", async ( {body, auth} ) => {
+    const {sender, text} = body
+    const { roomId } = auth
+
+    const roomExists = await redis.exists(`meta:${roomId}`)
+
+    if(!roomExists) {
+        throw new Error("The Room Does Not Exist")
+    }
+}, {
+    query: z.object({roomId: z.string()})
+    body: z.object({
+        sender: z.string().max(100),
+        text: z.string().max(1000),
+    })
+})
+
+const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages)
     .get('/', 'Hello Nextjs')
     .post('/', ({ body }) => body, {
         body: t.Object({
